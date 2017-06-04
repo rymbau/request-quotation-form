dce17cf067462cc8f9839183164230dbd4bd3d02
Rym BOUCHAGOUR <rym.bouchagour@glicer.com>
2017-06-04 18:30:09 +0200

    add validate

--- js/requestquotationform.js
+++ js/requestquotationform.js
@@ -1,31 +1,61 @@
 var formParameters = [
-    {id: 1, label: 'Nom', type: 'input' },
-    {id: 2, label: 'E-mail', type: 'input'},
-    {id: 3, label: 'Intitulé', type: 'input'},
-    {id: 4, label: 'Desciption', type: 'textarea', placeholder: 'secteur d\'activité, technologies, ...'},
-    {id: 5, label: 'Objectifs', type: 'textarea'}
+    {id: 'sct1', title: 'Contact', questions: [
+        {id: 1, label: 'Nom', control: 'input', placeholder: 'Nom Prénom', validate: 'required|alpha|min:2', icon: "icon-user-1" },
+        {id: 2, label: 'E-mail', control: 'input', placeholder: 'spongebob@exemple.fr', validate: 'required|email', icon: "icon-mail"}
+    ]},
+    {id: 'sect2', title: 'Projet', questions: [
+        {id: 3, label: 'Intitulé', control: 'input', placeholder: 'titre', validate: 'required|min:2', icon: "icon-tag-1"},
+        {id: 4, label: 'Description', control: 'textarea', placeholder: 'secteur d\'activité, technologies, ...', validate: 'required|min:2', icon: "icon-doc-inv"},
+        {id: 5, label: 'Objectifs', control: 'textarea', placeholder: 'liste des objectifs', validate: 'required|min:2', icon: "icon-target-1"},
+        {id: 7, label: 'Documents', control: 'input-file', icon: "icon-folder"}
+    ]}
 ];
+
+function questionTemplate(customField) {
+    return '<div :class="{\'input\':true,\'has-error\':errors.has(question.label)}">' +
+        customField +
+        '<span v-show="errors.has(question.label)" class="small help-block">{{errors.first(question.label)}}</span></div>';
+}
 
 Vue.component('form-input', {
     props: ['question'],
-    template: '<input :id="question.id" :name="question.label" class="form-control" type="text" ' +
-        'v-model="question.answer" :placeholder="question.placeholder"/>'
+    template: questionTemplate('<input :id="question.id" :name="question.label" ' +
+        ':class="{\'input\':true,\'has-error\':errors.has(question.label)}" ' +
+        'class="form-control" type="text" ' +
+        'v-model="question.answer" :placeholder="question.placeholder" v-validate="question.validate" />' +
+        '<label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}} </label>')
+});
+
+Vue.component('form-input-file', {
+    props: ['question'],
+    template: '<span class="btn btn-default btn-file">Parcourir ... <input :id="question.id" :name="question.label" class="form-control" type="file" ' +
+        ':placeholder="question.placeholder" hidden/></span>'
 });
 
 Vue.component('form-textarea', {
     props: ['question'],
-    template: '<textarea :id="question.id" :name="question.id" class="form-control" type="text" ' +
-        'v-model="question.answer" :placeholder="question.placeholder"/>'
+    template: questionTemplate('<textarea :id="question.id" :name="question.label" class="form-control" ' +
+        ':class="{\'input\':true,\'has-error\':errors.has(question.label)}" ' +
+        'v-model="question.answer" :placeholder="question.placeholder" v-validate="question.validate" />' +
+        '<label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}} </label>')
 });
 
 Vue.component('form-question', {
     props: ['question'],
     created: function () {
-        this.$options.template = '<div class="form-group">' +
-            '<label :for="question.id" class="control-label">{{question.label}}</label>';
-        switch (this.question.type) {
+        this.$options.template = '<div class="form-group">';
+        /* this.$options.template += '<label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}}';
+         if (this.question.required || ((this.question.validate !== undefined) && this.question.validate.match("required"))) {
+         this.$options.template += '<em>*</em>';
+         }
+         this.$options.template += '</label>';*/
+        switch (this.question.control) {
             case 'input':
                 this.$options.template += '<form-input :question="question"></form-input>';
+                break;
+            case 'input-file':
+                this.$options.template += '<div class="input"><label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}} </label>';
+                this.$options.template += '<form-input-file :question="question"></form-input-file></div>';
                 break;
             case 'textarea':
                 this.$options.template += '<form-textarea :question="question"></form-textarea>';
@@ -35,12 +65,34 @@ Vue.component('form-question', {
     }
 });
 
+Vue.component('form-section', {
+    props: ['section'],
+    created: function () {
+        this.$options.template = '<div class="section row"><h3 class="text-center">{{section.title}}</h3>' +
+            '<div class="col-md-12"><form-question v-for="question in section.questions" :question="question" :key="question.id">' +
+            ' </form-question></div></div>'
+    }
+});
+
+const config = {
+    locale: 'fr'
+};
+
+Vue.use(VeeValidate, config);
+
 var app = new Vue({
     el: '#quotationForm',
     data: {
-        questions: formParameters
-    }/*,
-     created: function () {
+        sections: formParameters
+    }
+    /*  created: function () {
      this.questions.push(formParameters);
-     }*/
+     }*/,
+    methods: {
+        submitForm: function (event) {
+
+        }
+    }
 });
+
+
