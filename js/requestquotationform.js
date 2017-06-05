d9a8200f97158442ea1d2fd4e15a0426ae0c91be
Rym BOUCHAGOUR <rym.bouchagour@glicer.com>
2017-06-05 19:07:10 +0200

    add submit form

--- js/requestquotationform.js
+++ js/requestquotationform.js
@@ -14,6 +14,8 @@ var formParameters = [
 function questionTemplate(customField) {
     return '<div :class="{\'input\':true,\'has-error\':errors.has(question.label)}">' +
         customField +
+        '<label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}} ' +
+        '<em v-show="question.validate">*</em></label>' +
         '<span v-show="errors.has(question.label)" class="small help-block">{{errors.first(question.label)}}</span></div>';
 }
 
@@ -22,39 +24,45 @@ Vue.component('form-input', {
     template: questionTemplate('<input :id="question.id" :name="question.label" ' +
         ':class="{\'input\':true,\'has-error\':errors.has(question.label)}" ' +
         'class="form-control" type="text" ' +
-        'v-model="question.answer" :placeholder="question.placeholder" v-validate="question.validate" />' +
-        '<label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}} </label>')
+        'v-model="question.answer" :placeholder="question.placeholder" v-validate="question.validate" />')
 });
 
 Vue.component('form-input-file', {
     props: ['question'],
-    template: '<span class="btn btn-default btn-file">Parcourir ... <input :id="question.id" :name="question.label" class="form-control" type="file" ' +
-        ':placeholder="question.placeholder" hidden/></span>'
+    template: '<span class="btn btn-default btn-file">Parcourir ... ' +
+        '<input :id="question.id" :name="question.label" class="form-control" type="file" ' +
+        ':placeholder="question.placeholder" accept=".xls,image/*,.doc,.ppt,.txt,.pdf" hidden ' +
+        '@change="filesChange($event.target.files)"' +
+        '/></span>',
+    methods: {
+        filesChange: function (fileList) {
+            if (!fileList.length) return;
+            console.log(fileList.length + " " + fileList[0].name);
+            this.question.answer = [
+                {'name': fileList[0].name, 'file': fileList[0]}
+            ];
+        }
+    }
 });
 
 Vue.component('form-textarea', {
     props: ['question'],
     template: questionTemplate('<textarea :id="question.id" :name="question.label" class="form-control" ' +
         ':class="{\'input\':true,\'has-error\':errors.has(question.label)}" ' +
-        'v-model="question.answer" :placeholder="question.placeholder" v-validate="question.validate" />' +
-        '<label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}} </label>')
+        'v-model="question.answer" :placeholder="question.placeholder" v-validate="question.validate" />')
 });
 
 Vue.component('form-question', {
     props: ['question'],
     created: function () {
         this.$options.template = '<div class="form-group">';
-        /* this.$options.template += '<label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}}';
-         if (this.question.required || ((this.question.validate !== undefined) && this.question.validate.match("required"))) {
-         this.$options.template += '<em>*</em>';
-         }
-         this.$options.template += '</label>';*/
         switch (this.question.control) {
             case 'input':
                 this.$options.template += '<form-input :question="question"></form-input>';
                 break;
             case 'input-file':
-                this.$options.template += '<div class="input"><label :for="question.id" class="control-label"><i :class="question.icon"></i> {{question.label}} </label>';
+                this.$options.template += '<div class="input"><label :for="question.id" class="control-label">'+
+                    '<i :class="question.icon"></i> {{question.label}} </label>';
                 this.$options.template += '<form-input-file :question="question"></form-input-file></div>';
                 break;
             case 'textarea':
@@ -84,13 +92,46 @@ var app = new Vue({
     el: '#quotationForm',
     data: {
         sections: formParameters
-    }
-    /*  created: function () {
-     this.questions.push(formParameters);
-     }*/,
+    },
     methods: {
         submitForm: function (event) {
+            var $this = this;
+            var $validator = this.$validator;
+            var data = {};
+            this.sections.forEach(function (section) {
+                section.questions.forEach(function (question) {
+                    if (question.validate !== undefined) {
+                        $validator.attach(question.label, question.validate);
+                        data[question.label] = question.answer;
+                    }
+                });
+            });
 
+            var $sections = this.sections;
+            $validator.validateAll(data).then(function () {
+                var form = [];
+                $sections.forEach(function (section) {
+                    section.questions.forEach(function (question) {
+                        form.push({
+                            id: question.id,
+                            label: question.label,
+                            answer: question.answer
+                        })
+                    });
+                });
+                console.log(JSON.stringify(form));
+            }).catch(function (error) {
+                    $this.$children.forEach(function (child) {
+                        child.$children.forEach(function (child) {
+                            child.$children.forEach(function (child) {
+                                child.$validator.validateAll().then(function () {
+                                }).catch(function () {
+                                    });
+                            });
+                        });
+                    });
+                    console.log('Invalid form. Error count : ' + $validator.getErrors().count());
+                });
         }
     }
 });
